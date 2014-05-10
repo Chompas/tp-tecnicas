345c9fa660989085754b985d6ce289e21c90a57f
María Inés Parnisari <maineparnisari@gmail.com>
2014-05-10 17:18:17 -0300

    Probando el loader de properties con un archivo "config.xml"

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class Main {
+
+		public static int main(String[] args)
+			throws Exception
+		{
+			Properties properties = new Properties();
+			properties.loadFromXML(new FileInputStream("config.xml"));
+			
+			String logLevel = properties.getProperty("LogLevel");
+			String separator = properties.getProperty("Separator");
+
+			System.out.println(logLevel);
+			System.out.println(separator);
+			
+			return 0;
+		}
+}
