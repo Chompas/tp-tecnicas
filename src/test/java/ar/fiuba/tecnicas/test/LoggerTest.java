5f62168fe0f94235a816a2dc69c533ff56ce6803
María Inés Parnisari <maineparnisari@gmail.com>
2014-05-14 00:48:02 -0300

    Agrego test para Logger::log con formato

--- src/test/java/ar/fiuba/tecnicas/test/LoggerTest.java
+++ src/test/java/ar/fiuba/tecnicas/test/LoggerTest.java
@@ -1,48 +1,59 @@
 package ar.fiuba.tecnicas.test;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-import java.util.logging.Level;
-
-import junit.framework.Assert;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import ar.fiuba.tecnicas.logging.*;
 import ar.fiuba.tecnicas.logging.config.LogLevel;
+import ar.fiuba.tecnicas.logging.formatter.LogFormatter;
 import ar.fiuba.tecnicas.logging.handlers.ConsoleHandler;
-import ar.fiuba.tecnicas.logging.handlers.FileHandler;
 
 public class LoggerTest {
-
-	@Test
-	public void integrationTest() {
-		Logger logger = new Logger(null);
-		logger.addHandler(new ConsoleHandler());
-		
-		FileHandler fileHandler = new FileHandler();
-		fileHandler.setFilename("test.log");
-		logger.addHandler(fileHandler);
-		
-		logger.log("Test", LogLevel.WARN);
+	
+	private Logger logger;
+	private String message;
+	private String formattedMessage;
+	
+	@Before
+	public void iniatilize()
+	{
+		logger = new Logger(null);
+		message = "simple message";
+		formattedMessage = "simple message - ERROR";
 	}
 	
 	@Test
 	public void simpleLog()
-	{
-		Logger logger = new Logger(null);
-		
-		ConsoleHandler mockedConsoleHandler = mock(ConsoleHandler.class);
-		
+	{	
+		// arrange
+		ConsoleHandler mockedConsoleHandler = mock(ConsoleHandler.class);		
 		logger.addHandler(mockedConsoleHandler);
 		
 		// act
-		logger.log("simple message", LogLevel.ERROR);
+		logger.log(message, LogLevel.ERROR);
 		
 		// assert
-		Mockito.verify(mockedConsoleHandler).write("simple message");
+		Mockito.verify(mockedConsoleHandler).write(message);
+	}
+	
+	@Test
+	public void formattedLog()
+	{
+		// arrange		
+		LogFormatter mockedFormatter = mock (LogFormatter.class);		
+		when(mockedFormatter.format(message, LogLevel.ERROR)).thenReturn(formattedMessage);
+		
+		ConsoleHandler mockedConsoleHandler = mock(ConsoleHandler.class);		
+		logger.addHandler(mockedConsoleHandler);
+		
+		// act
+		logger.log(message, LogLevel.ERROR, mockedFormatter);
+		
+		// assert
+		Mockito.verify(mockedConsoleHandler).write(formattedMessage);
 	}
 
 }
