381f67afc4f034b6f23722e7aa05ae19a52a3daf
María Inés Parnisari <maine_parnisari@hotmail.com>
2014-05-15 00:03:00 -0300

    Agrego clase Filter y sus tests

---
+++ src/main/java/ar/fiuba/tecnicas/logging/Filter.java
@@ -0,0 +1,31 @@
+package ar.fiuba.tecnicas.logging;
+
+import ar.fiuba.tecnicas.logging.config.LogLevel;
+
+public class Filter {
+	
+	private LogLevel globalLogLevel;
+	
+	public Filter(LogLevel globalLogLevel)
+	{
+		this.globalLogLevel = globalLogLevel;
+	}
+	
+	public String filter(String message, LogLevel level)
+	{
+		if (shouldShowMessage(level))
+		{
+			return message;
+		}
+		else
+		{
+			return "";
+		}
+	}
+	
+	private boolean shouldShowMessage(LogLevel level) {
+		return (level.getCode() <= globalLogLevel.getCode()); 
+	}
+	
+
+}
