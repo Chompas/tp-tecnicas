999290c50c30a803856914f2673a0fd38019e0ed
Emiliano <chompas@gmail.com>
2014-06-03 15:46:38 -0300

    Agregando clase LogFormatter

--- src/main/java/ar/fiuba/tecnicas/logging/formatter/LogFormatter.java
+++ src/main/java/ar/fiuba/tecnicas/logging/formatter/LogFormatter.java
@@ -1,11 +1,5 @@
 package ar.fiuba.tecnicas.logging.formatter;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import ar.fiuba.tecnicas.logging.config.LogLevel;
 
 public class LogFormatter implements ILogFormatter {
@@ -13,7 +7,7 @@ public class LogFormatter implements ILogFormatter {
 	private String format;
 	private String separator;
 	
-	private String dateRegex = Pattern.quote("%d{") + "(.*?)" + Pattern.quote("}");
+	
 
 	public LogFormatter(String format, String separator) {
 		super();
@@ -36,53 +30,9 @@ public class LogFormatter implements ILogFormatter {
 	@Override
 	public String format(String message, LogLevel level) {
 
-		/****************
-		 * FORMATOS *****************
-		 * %d{xxxxxxx} debería aceptar cualquier formato
-		 *  válido de SimpleDateFormat. 
-		 * %p debería mostrar el Nivel del mensaje. 
-		 * %t deberia mostrar el nombre del thread actual. 
-		 * %m debería mostrar el contenido del mensaje logueado por el usuario. 
-		 * %% debería mostrar un % (es el escape de %). 
-		 * %n debería mostrar el “separador
-		 * ” con el que el usuario configuró la herramienta
-		 *  o un default a elección.  %L line number.  %F filename. 
-		 * %M method name. 
-		 ******************************************/
 
-		DateFormat dateFormat = new SimpleDateFormat(this.getDateFormat());
-		Date d = new Date();
-		String date = dateFormat.format(d);
-
-		String threadName = Thread.currentThread().getName();
-		String separator = this.separator;
-		String lineNumber = FormatterHelper.getCallingLineNumber();
-		String filename = FormatterHelper.getCallingFilename();
-		String methodName = FormatterHelper.getCallingMethod();
-
-		String formattedMessage = this.format;
-		formattedMessage = formattedMessage.replaceAll(this.dateRegex, date);
-		formattedMessage = formattedMessage.replace("%p", level.name());
-		formattedMessage = formattedMessage.replace("%t", threadName);
-		formattedMessage = formattedMessage.replace("%m", message);
-		formattedMessage = formattedMessage.replace("%%", "%");
-		formattedMessage = formattedMessage.replace("%n", separator);
-		formattedMessage = formattedMessage.replace("%L", lineNumber);
-		formattedMessage = formattedMessage.replace("%F", filename);
-		formattedMessage = formattedMessage.replace("%M", methodName);
-
-		return formattedMessage;
 	}
 	
-	private String getDateFormat() {
-		Pattern pattern = Pattern.compile(this.dateRegex);
-		Matcher matcher = pattern.matcher(this.format);
 
-		String textInBetween = "";
-		while (matcher.find()) {
-		  textInBetween = matcher.group(1); 
-		}	
-		return textInBetween;	
-	}
 
 }
---
+++ src/main/java/ar/fiuba/tecnicas/logging/formatter/LogMessage.java
@@ -0,0 +1,103 @@
+package ar.fiuba.tecnicas.logging.formatter;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import ar.fiuba.tecnicas.logging.config.LogLevel;
+
+public class LogMessage {
+	
+	private String plainMessage;
+	private HashMap<String, String> hashMessage = new HashMap<String, String>();
+	private String dateRegex = Pattern.quote("%d{") + "(.*?)" + Pattern.quote("}");
+	private String date;
+	private String threadName;
+	private String lineNumber;
+	private String filename;
+	private String methodName;
+	
+	public LogMessage(String format, String separator, String message, LogLevel logLevel) {
+		super();
+		
+		DateFormat dateFormat = new SimpleDateFormat(this.getDateFormat(format));
+		Date d = new Date();
+		this.date = dateFormat.format(d);
+
+		this.threadName = Thread.currentThread().getName();
+		this.lineNumber = FormatterHelper.getCallingLineNumber();
+		this.filename = FormatterHelper.getCallingFilename();
+		this.methodName = FormatterHelper.getCallingMethod();
+		
+		this.setupPlainMessage(format, separator, message, logLevel);
+		this.setupHashMessage(format, separator, message, logLevel);
+		
+	}
+	
+	public String getPlainMessage() {
+		return this.plainMessage;
+	}
+	
+	public HashMap<String, String> getHashMessage() {
+		return this.hashMessage;
+	}
+	
+	private void setupHashMessage(String format, String separator, String message, LogLevel logLevel) {
+		
+		hashMessage.put("level", logLevel.name());
+		hashMessage.put("threadName", this.threadName);
+		hashMessage.put("message", message);
+		hashMessage.put("separator", separator);
+		hashMessage.put("lineNumber", this.lineNumber);
+		hashMessage.put("filename", this.filename);
+		hashMessage.put("methodName", this.methodName);
+	}
+	
+	private void setupPlainMessage(String format, String separator, String message, LogLevel logLevel) {
+		
+		/****************
+		 * FORMATOS *****************
+		 * %d{xxxxxxx} debería aceptar cualquier formato
+		 *  válido de SimpleDateFormat. 
+		 * %p debería mostrar el Nivel del mensaje. 
+		 * %t deberia mostrar el nombre del thread actual. 
+		 * %m debería mostrar el contenido del mensaje logueado por el usuario. 
+		 * %% debería mostrar un % (es el escape de %). 
+		 * %n debería mostrar el “separador
+		 * ” con el que el usuario configuró la herramienta
+		 *  o un default a elección.  %L line number.  %F filename. 
+		 * %M method name. 
+		 ******************************************/
+
+
+
+		String formattedMessage = format;
+		formattedMessage = formattedMessage.replaceAll(this.dateRegex, this.date);
+		formattedMessage = formattedMessage.replace("%p", logLevel.name());
+		formattedMessage = formattedMessage.replace("%t", this.threadName);
+		formattedMessage = formattedMessage.replace("%m", message);
+		formattedMessage = formattedMessage.replace("%%", "%");
+		formattedMessage = formattedMessage.replace("%n", separator);
+		formattedMessage = formattedMessage.replace("%L", this.lineNumber);
+		formattedMessage = formattedMessage.replace("%F", this.filename);
+		formattedMessage = formattedMessage.replace("%M", this.methodName);
+
+		this.plainMessage = formattedMessage;
+	}
+	
+	private String getDateFormat(String format) {
+		Pattern pattern = Pattern.compile(this.dateRegex);
+		Matcher matcher = pattern.matcher(format);
+
+		String textInBetween = "";
+		while (matcher.find()) {
+		  textInBetween = matcher.group(1); 
+		}	
+		return textInBetween;	
+	}
+	
+
+}
