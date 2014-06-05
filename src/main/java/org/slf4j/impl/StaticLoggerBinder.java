e51d80c614f600db1564d49aed40cdd548ec719f
javierlara <javi29c@hotmail.com>
2014-06-04 23:29:03 -0300

    Subo estructura para la implementacion del adapter de slf4j

---
+++ src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -0,0 +1,20 @@
+package org.slf4j.impl;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.spi.LoggerFactoryBinder;
+
+public class StaticLoggerBinder implements LoggerFactoryBinder {
+
+	@Override
+	public ILoggerFactory getLoggerFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLoggerFactoryClassStr() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
