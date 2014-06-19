ffeece6c610b04a5b679c53ce806c836b915f661
Emiliano <chompas@gmail.com>
2014-06-18 23:20:21 -0300

    Separando los test de filtros custom de FilterTest

--- src/test/java/ar/fiuba/tecnicas/test/CustomFilterTest.java
+++ src/test/java/ar/fiuba/tecnicas/test/CustomFilterTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Date;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import ar.fiuba.tecnicas.logging.config.LogLevel;
@@ -13,31 +12,32 @@ import ar.fiuba.tecnicas.logging.filters.CustomFilter;
 import ar.fiuba.tecnicas.logging.filters.Filter;
 import ar.fiuba.tecnicas.logging.formatter.TextLogFormatter;
 
-public class FilterTest {
+public class CustomFilterTest {
 	
 	private Filter filter;
 	private static String message = "test";
 	private CustomFilter customFilter = new CustomFilter();
 	private TextLogFormatter formatter;
 	
-	@Before
-	public void setUp() {
-		this.formatter = new TextLogFormatter("%m", "");
-	}
-	
 	@Test
-	public void returnsEmptyMessageIfLevelIsLower() {
-		this.filter = new Filter(LogLevel.ERROR);
+	public void customFilterWithFutureFromDate() {
+		this.filter = new Filter(LogLevel.DEBUG);
+		Date today = new Date();
+		Date tomorrow = new Date(today.getTime() + (1000 * 60 * 60 * 24));
+		this.customFilter.fromDate = tomorrow;
 		
-		LogMessage logMessage = new LogMessage(new Date(), formatter, message, LogLevel.DEBUG);
-		String filteredMessage = this.filter.filter(logMessage, LogLevel.DEBUG,"", customFilter);
+		LogMessage logMessage = new LogMessage(new Date(), formatter, message, LogLevel.ERROR);
+		String filteredMessage = this.filter.filter(logMessage, LogLevel.ERROR,"", customFilter);
 		
 		assertEquals(filteredMessage, "");
 	}
 	
 	@Test
-	public void returnsStringIfLevelIsHigher() {	
+	public void customFilterWithPastFromDate() {
 		this.filter = new Filter(LogLevel.DEBUG);
+		Date today = new Date();
+		Date yesterday = new Date(today.getTime() - (1000 * 60 * 60 * 24));
+		this.customFilter.fromDate = yesterday;
 		
 		LogMessage logMessage = new LogMessage(new Date(), formatter, message, LogLevel.ERROR);
 		String filteredMessage = this.filter.filter(logMessage, LogLevel.ERROR,"", customFilter);
